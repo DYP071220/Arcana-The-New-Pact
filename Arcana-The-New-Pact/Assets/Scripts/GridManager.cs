926e54faa4a74a53c97d96fce6826ac3eb3ed83f
HlllllC <2260194477@qq.com>
2026-02-12 17:27:32 +0800

    Update random map

--- Arcana-The-New-Pact/Assets/Scripts/GridManager.cs
+++ Arcana-The-New-Pact/Assets/Scripts/GridManager.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     [SerializeField]private int width, height;
-    [SerializeField]private Tile tilePrefab;
+    [SerializeField]private Tile wallTile,floorTile;
     [SerializeField]private Transform camera;
 
-
+    private Dictionary<Vector2, Tile> tiles;
 
     private void Start()
     {
@@ -17,15 +17,28 @@ public class GridManager : MonoBehaviour
 
     void GenerateGrid()
     {
+        tiles = new Dictionary<Vector2, Tile>();
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                var spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity);
+                var randomNumber = Random.Range(0, 10);
+                var randomTile = new Tile();
+                if (randomNumber > 8)
+                {
+                    randomTile = wallTile;
+                }
+                else
+                {
+                    randomTile = floorTile;
+                }
+                var spawnedTile = Instantiate(randomTile, new Vector3(x, y), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
 
                 var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
                 spawnedTile.Init(isOffset);
+
+                tiles[new Vector2(x, y)] = spawnedTile;
             }
         }
 
@@ -35,5 +48,14 @@ public class GridManager : MonoBehaviour
 
 
 
+    public Tile GetTile(Vector2 pos)
+    {
+        if (tiles.TryGetValue(pos, out var tile))
+        {
+            return tile;
+        }
+
+        return null;
+    }
 
 }
